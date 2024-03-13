3b4c622c23d3ea3584640e8d0f387dcef23dafc6
kunst1080 <kontrapunk1080@gmail.com>
2024-03-13 18:31:50 +0900

    feat: implement rename

--- awsfs/rename.go
+++ awsfs/rename.go
@@ -13,34 +13,25 @@ func (s Server) Rename(ctx context.Context, oldPath, newPath string) error {
 		return os.ErrInvalid
 	}
 
-	// Referenceの取得
-	ref, err := s.MetadataStore.GetReference(ctx, oldPath)
+	ref, err := s.MetadataStore.GetReference(ctx, referenceID)
 	if err != nil {
 		return err
 	}
 
-	// oldpathの項目がなければエラー
 	id, ok := ref.Entries[oldPath]
 	if ok {
 		return os.ErrNotExist
 	}
 
-	// enrtyの取得
 	entry, err := s.MetadataStore.GetEntry(ctx, id)
 	if err != nil {
 		return err
 	}
 
-	// Referenceに新しいキーの値を追加
 	ref.Entries[newPath] = id
-
-	// Referenceから古いキーの値を削除
 	delete(ref.Entries, oldPath)
-
-	// entryのnameを書き換え
 	entry.Name = newPath
 
-	// referenceとentryの更新
 	err = s.MetadataStore.UpdateEntryName(ctx, entry, ref)
 	if err != nil {
 		return err
