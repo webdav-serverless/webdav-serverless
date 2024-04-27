cf949982ee1813331f9be61bb5cb6882647df2f8
vvatanabe <yuichi.watanabe.ja@gmail.com>
2024-04-27 17:19:59 +0900

    feat: Refactor Create function in awsfs/create.go to improve code readability and maintainability

--- awsfs/create.go
+++ awsfs/create.go
@@ -11,17 +11,6 @@ import (
 	"github.com/google/uuid"
 )
 
-type SizeReader struct {
-	io.Reader
-	Size int64
-}
-
-func (r *SizeReader) Read(p []byte) (n int, err error) {
-	n, err = r.Reader.Read(p)
-	r.Size += int64(n)
-	return
-}
-
 func (s Server) Create(ctx context.Context, path string, flag int, perm os.FileMode, r io.Reader) (os.FileInfo, error) {
 
 	ref, err := s.MetadataStore.GetReference(ctx, referenceID)
@@ -29,47 +18,74 @@ func (s Server) Create(ctx context.Context, path string, flag int, perm os.FileM
 		return nil, err
 	}
 
-	//_, ok := ref.Entries[path]
-	//if ok {
-	//	return nil, os.ErrExist
-	//}
-
-	parentDirPath := GetParentDirPath(path)
-
-	parentID, ok := ref.Entries[parentDirPath]
-	if !ok {
-		return nil, errors.New("no such parent directory")
+	entryID, shouldUpdate := ref.Entries[path]
+	if !shouldUpdate {
+		entryID = uuid.New().String()
 	}
 
-	sr := &SizeReader{Reader: r}
-
-	entryID := uuid.New().String()
+	sr := &sizingReader{Reader: r}
 
 	err = s.PhysicalStore.PutObjectLarge(ctx, entryID, sr)
 	if err != nil {
 		return nil, err
 	}
 
-	newEntry := Entry{
-		ID:       entryID,
-		ParentID: parentID,
-		Name:     filepath.Base(path),
-		Type:     EntryTypeFile,
-		Size:     sr.Size,
-		Modify:   time.Now(),
-		Version:  1,
+	if shouldUpdate {
+		entry, err := s.MetadataStore.GetEntry(ctx, entryID)
+		if err != nil {
+			return nil, err
+		}
+		entry.Size = sr.size
+		entry.Modify = time.Now()
+		err = s.MetadataStore.UpdateEntry(ctx, entry)
+		if err != nil {
+			return nil, err
+		}
+		return &FileInfo{
+			name:    entry.Name,
+			size:    entry.Size,
+			mode:    perm,
+			modTime: entry.Modify,
+			isDir:   false,
+			sys:     nil,
+		}, nil
+	} else {
+		parentDirPath := filepath.Dir(path)
+		parentID, ok := ref.Entries[parentDirPath]
+		if !ok {
+			return nil, errors.New("no such parent directory")
+		}
+		newEntry := Entry{
+			ID:       entryID,
+			ParentID: parentID,
+			Name:     filepath.Base(path),
+			Type:     EntryTypeFile,
+			Size:     sr.size,
+			Modify:   time.Now(),
+			Version:  1,
+		}
+		err = s.MetadataStore.AddEntry(ctx, newEntry, path)
+		if err != nil {
+			return nil, err
+		}
+		return &FileInfo{
+			name:    newEntry.Name,
+			size:    newEntry.Size,
+			mode:    perm,
+			modTime: newEntry.Modify,
+			isDir:   false,
+			sys:     nil,
+		}, nil
 	}
-	err = s.MetadataStore.AddEntry(ctx, newEntry, path)
-	if err != nil {
-		return nil, err
-	}
+}
 
-	return &FileInfo{
-		name:    newEntry.Name,
-		size:    newEntry.Size,
-		mode:    perm,
-		modTime: newEntry.Modify,
-		isDir:   false,
-		sys:     nil,
-	}, nil
+type sizingReader struct {
+	io.Reader
+	size int64
+}
+
+func (r *sizingReader) Read(p []byte) (n int, err error) {
+	n, err = r.Reader.Read(p)
+	r.size += int64(n)
+	return
 }
